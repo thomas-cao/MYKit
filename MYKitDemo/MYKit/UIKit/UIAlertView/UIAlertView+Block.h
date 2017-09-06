96942fd2090540e1ec33cac2733fc5ee33909c74
sunjinshuai <s_mike@163.com>
2017-09-06 17:12:06 +0800

    UIAlertView扩展

---
+++ MYKitDemo/MYKit/UIKit/UIAlertView/UIAlertView+Block.h
@@ -0,0 +1,39 @@
+//
+//  UIAlertView+Block.h
+//  MYKitDemo
+//
+//  Created by sunjinshuai on 2017/9/6.
+//  Copyright © 2017年 com.51fanxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef void (^UIAlertViewDismissWithButtonBlock)(NSInteger buttonIndex);
+typedef void (^UIAlertViewCancelBlock)(void);
+
+@interface UIAlertView (Block)
+
++ (UIAlertView *)alertViewWithTitle:(NSString *)title
+message:(NSString *)message
+                  cancelButtonTitle:(NSString *)cancelButtonTitle;
+
++ (UIAlertView *)alertViewWithTitle:(NSString *)title
+                            message:(NSString *)message
+                  cancelButtonTitle:(NSString *)cancelButtonTitle
+                  otherButtonTitles:(NSArray *)otherButtonTitles
+      didDismissConfirmButtionBlock:(UIAlertViewDismissWithButtonBlock)confirmButtionBlock
+        didDismissCancelButtonBlock:(UIAlertViewCancelBlock)cancelButtonBlock;
+
++ (UIAlertView *)alertViewWithTitle:(NSString *)title
+                            message:(NSString *)message
+                  cancelButtonTitle:(NSString *)cancelButtonTitle
+                  otherButtonTitles:(NSArray *)otherButtonTitles
+                           subViews:(NSArray *)subViews
+      didDismissConfirmButtionBlock:(UIAlertViewDismissWithButtonBlock)confirmButtionBlock
+        didDismissCancelButtonBlock:(UIAlertViewCancelBlock)cancelButtonBlock;
+
+@end
+
+NS_ASSUME_NONNULL_END
