6d6a362c769071fbfec161bb1f385ab79d192d70
sunjinshuai <s_mike@163.com>
2017-09-06 16:38:51 +0800

    项目搭建

---
+++ MYKit/MYKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MYKit
+//
+//  Created by sunjinshuai on 2017/9/6.
+//  Copyright © 2017年 com.51fanxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MYKit/MYKit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MYKit
+//
+//  Created by sunjinshuai on 2017/9/6.
+//  Copyright © 2017年 com.51fanxing. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
