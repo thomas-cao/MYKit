56cc995062e77a1778dc57090b0cf3bfc0fd9697
sunjinshuai <s_mike@163.com>
2017-09-07 19:38:48 +0800

    UIScrollview

---
+++ MYKitDemo/MYKit/UIKit/UIScrollView/UIScrollView+Extension.m
@@ -0,0 +1,53 @@
+//
+//  UIScrollView+Extension.m
+//  MYKitDemo
+//
+//  Created by sunjinshuai on 2017/9/7.
+//  Copyright © 2017年 com.51fanxing. All rights reserved.
+//
+
+#import "UIScrollView+Extension.h"
+
+@implementation UIScrollView (Extension)
+
+- (void)scrollToTop {
+    [self scrollToTopAnimated:YES];
+}
+
+- (void)scrollToBottom {
+    [self scrollToBottomAnimated:YES];
+}
+
+- (void)scrollToLeft {
+    [self scrollToLeftAnimated:YES];
+}
+
+- (void)scrollToRight {
+    [self scrollToRightAnimated:YES];
+}
+
+- (void)scrollToTopAnimated:(BOOL)animated {
+    CGPoint off = self.contentOffset;
+    off.y = 0 - self.contentInset.top;
+    [self setContentOffset:off animated:animated];
+}
+
+- (void)scrollToBottomAnimated:(BOOL)animated {
+    CGPoint off = self.contentOffset;
+    off.y = self.contentSize.height - self.bounds.size.height + self.contentInset.bottom;
+    [self setContentOffset:off animated:animated];
+}
+
+- (void)scrollToLeftAnimated:(BOOL)animated {
+    CGPoint off = self.contentOffset;
+    off.x = 0 - self.contentInset.left;
+    [self setContentOffset:off animated:animated];
+}
+
+- (void)scrollToRightAnimated:(BOOL)animated {
+    CGPoint off = self.contentOffset;
+    off.x = self.contentSize.width - self.bounds.size.width + self.contentInset.right;
+    [self setContentOffset:off animated:animated];
+}
+
+@end
