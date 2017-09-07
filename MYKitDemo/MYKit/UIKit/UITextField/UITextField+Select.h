4722505c68c56097975cc4c5de80451fc770b8cb
sunjinshuai <s_mike@163.com>
2017-09-07 19:32:40 +0800

    UITextField

---
+++ MYKitDemo/MYKit/UIKit/UITextField/UITextField+Select.h
@@ -0,0 +1,25 @@
+//
+//  UITextField+Select.h
+//  MYKitDemo
+//
+//  Created by sunjinshuai on 2017/9/7.
+//  Copyright © 2017年 com.51fanxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextField (Select)
+
+/**
+ Set all text selected.
+ */
+- (void)selectAllText;
+
+/**
+ Set text in range selected.
+ 
+ @param range  The range of selected text in a document.
+ */
+- (void)setSelectedRange:(NSRange)range;
+
+@end
